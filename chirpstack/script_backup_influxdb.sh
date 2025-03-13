e1e5b2c1f4a435cf4e569fd579dc30faeff3b6d5
Thomas <160124815+thomas-univ-smb@users.noreply.github.com>
2025-03-13 13:00:05 +0100

    Update script_backup_influxdb.sh
    Nouvelle version

--- chirpstack/script_backup_influxdb.sh
+++ chirpstack/script_backup_influxdb.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
-dest_folder="/var/backup_influxdb"
-logger "Backup influxdb to ${dest_folder}"
+
+# Ajoute le chemin complet pour les commandes
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+# Défini une variable pour le chemin d'acces au serveur
+SERVER_DIR="/home/almalinux/thomas/chirpstack"
+
+# Défini une variable pour le chemin d'acces a la sauvegarde
+DEST_FOLDER="/var/backup_influxdb"
+logger "Backup influxdb to ${DEST_FOLDER}"
 
 # Définition du token utiliser pour permettre la sauvegarde
 token="uk8yS3KzVkhaF0cf/CFWy044i8qpNTenEKGCA3SgWK8="
-rm -rf "${dest_folder}"
-sudo docker exec iot-app-influxdb-1 sh -c 'rm -rf /root/influx_backup'
-sudo docker exec iot-app-influxdb-1 influx backup /root/influx_backup -t "${token}"
 
-# Copie des sauvegardes dans le dossier dest_folder
-sudo docker cp iot-app-influxdb-1:/root/influx_backup "${dest_folder}"
-sudo chown -R almalinux "${dest_folder}"
+# Supprime l'ancienne sauvegarde
+rm -rf "${DEST_FOLDER}"
+
+# Changement de dossier pour celui du docker compose afin d'executer la commande docker
+cd "$SERVER_DIR" || exit 1
+
+# Effectue la nouvelle sauvegarde
+/usr/bin/docker exec iot-app-influxdb-1 sh -c 'rm -rf /root/influx_backup'
+/usr/bin/docker exec iot-app-influxdb-1 influx backup /root/influx_backup -t "${token}"
+
+# Copie les sauvegardes dans le dossier DEST_FOLDER
+sudo docker cp iot-app-influxdb-1:/root/influx_backup "${DEST_FOLDER}"
+sudo chown -R almalinux "${DEST_FOLDER}"
+echo "Fichiers influxdb copiés dans le dossier actuel."
