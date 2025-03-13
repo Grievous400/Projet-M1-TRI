fdfe0d63444d3ce90c2d71aee9a6430b28d9fb3a
Thomas <160124815+thomas-univ-smb@users.noreply.github.com>
2025-03-13 13:23:01 +0100

    Update script_restore_influxdb.sh

--- chirpstack/script_restore_influxdb.sh
+++ chirpstack/script_restore_influxdb.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-dest_folder="/var/backup_influxdb"
-logger "Restore influxdb with ${dest_folder}"
+
+# Ajoute le chemin complet pour les commandes
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+# Défini une variable pour le chemin d'acces a la sauvegarde
+DEST_FOLDER="/var/backup_influxdb"
+
+logger "Restore influxdb with ${DEST_FOLDER}"
 
 # Copie des sauvegardes dans le dossier /root/influx_backup
-sudo docker cp "${dest_folder}" iot-app-influxdb-1:/root/influx_backup
+/usr/bin/docker cp "${DEST_FOLDER}" iot-app-influxdb-1:/root/influx_backup
 
 # Restaure et remplaces les données déjà existante avec --full
-sudo docker exec iot-app-influxdb-1 influx restore /root/influx_backup --full
+/usr/bin/docker exec iot-app-influxdb-1 influx restore /root/influx_backup --full
