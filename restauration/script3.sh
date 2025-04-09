66a119d17fa8fd010bb72b1d3465a19071cf80c7
Thomas <160124815+thomas-univ-smb@users.noreply.github.com>
2025-04-09 22:02:44 +0200

    Create script3.sh

---
+++ restauration/script3.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+REMOTE_HOST="XXX.XXX.XXX.XXX"
+REMOTE_USER="almalinux"
+SSH_KEY_PATH="/home/etudiant/sshkeys/private_key_openssh"
+
+# Exécuter le script de configuration sur la machine distante
+echo "---------------------------------------"
+echo "Exécution du script de configuration."
+echo "---------------------------------------"
+ssh -i $SSH_KEY_PATH $REMOTE_USER@$REMOTE_HOST << EOF
+declare -A permissions=(
+    ["/home/almalinux/florent/iot-app/mosquitto-clients/config"]="1883|1883"
+    ["/home/almalinux/florent/iot-app/mosquitto-clients/config/mosquitto.conf"]="1883|1883"
+    ["/home/almalinux/florent/iot-app/mosquitto-clients/config/authorization.acl"]="1883|1883"
+    ["/home/almalinux/florent/iot-app/mosquitto-clients/config/passdb"]="1883|1883"
+    ["/home/almalinux/florent/iot-app/mosquitto-clients/config/certs"]="1883|1883"
+    ["/home/almalinux/florent/iot-app/mosquitto-clients/config/certs/cafile.pem"]="1883|1883"
+    ["/home/almalinux/florent/iot-app/mosquitto-clients/config/certs/certificate.pem"]="1883|1883"
+    ["/home/almalinux/florent/iot-app/mosquitto-clients/config/certs/privatekey.pem"]="1883|1883"
+    ["/home/almalinux/florent/iot-app/grafana/data/plugins"]="472|root"
+    ["/home/almalinux/florent/iot-app/grafana/data/grafana.db"]="472|root"
+    ["/home/almalinux/florent/iot-app/grafana/data/png"]="472|root"
+    ["/home/almalinux/florent/iot-app/grafana/data/csv"]="472|root"
+    ["/home/almalinux/florent/iot-app/grafana/data/pdf"]="472|root"
+    ["/home/almalinux/thomas/chirpstack/configuration/mosquitto"]="root|root"
+    ["/home/almalinux/thomas/chirpstack/configuration/mosquitto/config"]="1883|1883"
+    ["/home/almalinux/thomas/chirpstack/configuration/mosquitto/config/mosquitto.conf"]="1883|1883"
+)
+
+for path in "${!permissions[@]}"; do
+    owner_group=${permissions[$path]}
+    owner=$(echo "$owner_group" | cut -d'|' -f1)
+    group=$(echo "$owner_group" | cut -d'|' -f2)
+    echo "Modification de $path - Propriétaire: $owner, Groupe: $group"
+    sudo chown "$owner:$group" "$path"
+done
+EOF
+echo "---------------------------------------"
+echo "Script terminé."
+echo "---------------------------------------"
